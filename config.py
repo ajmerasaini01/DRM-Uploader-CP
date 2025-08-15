ffda1b9a9bc975894eabb170fe28fef0d069a15f
ajmerasaini01 <ajmerasaini01@gmail.com>
2025-08-15 21:07:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-# (c) ACE 
+# (c) ACE
 
 import os
 
 class Config(object):
-    # get a token from @BotFather
-    pass
-    """
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    API_ID = int(os.environ["API_ID", ]
-    API_HASH = os.environ["API_HASH", ""]
-    AUTH_USERS = """"
- 
+    # Telegram bot token from @BotFather
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7614269151:AAFpIPvjOxS9Lpdwag2Uw8Fh8P79YzDtWz8")
+
+    # Telegram API ID and Hash from https://my.telegram.org
+    API_ID = int(os.environ.get("API_ID", 24307077))
+    API_HASH = os.environ.get("API_HASH", "3fb755f9e1e27952e454661ff4ffae41")
+
+    # Authorized user IDs (comma-separated or single)
+    AUTH_USERS = set(int(x) for x in os.environ.get("AUTH_USERS", "5298142728").split(","))
